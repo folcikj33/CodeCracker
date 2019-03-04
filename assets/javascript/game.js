d3a9c0bdba763a0e0aa9d1270ab088d19063c542
Jake <folcik.jake@gmail.com>
2019-03-03 22:32:50 -0600

    as of sun march 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,43 @@
-var letters = "["
+//create an array of words
 
-console.log(log);
-console.log(letters);
-console.log(words)
-console.log(letters)
-console.log(guesses)
-console.log(wins)
-console.log(local)
-console.log(images)
-console.log(sound)
-console.log(array)
-console.log(guesses)
-console.log(win) 
-console.log(click)
-images
-sound
+var chalWords = [
+	"keyboard",
+	"cyber",
+	"nebular",
+	"stellar",
+	"hyperlink",
+	"password",
+	"database",
+	"security",
+	"firewall"
+];
+
+//choose word randomly from array
+var randWord = chalWords[Math.floor(Math.random() * chalWords.length)];
+
+//make underscores based on length of word
+var underscores = [];
+
+for (let i = 0; i < randWord.length; i++) {
+	underscores.push("_");
+}
+
+//get users guess
+document.addEventListener("keypress", event);
+
+console.log(event);
+
+
+//compare guess letter and selected word
+//if found in word push to correct spot in underscore array
+//if wrong push to junk-pile array
+//create wins and losses and guesses variables
+
+var wins = "Wins: " + 0;
+var losses = "Losses: " + 0;
+var guesses = "Guesses: " + 0;
+
+//attach them to display in document
+
+//if all letters correct add 1 to wins
+//if guesses run out add 1 to losses
