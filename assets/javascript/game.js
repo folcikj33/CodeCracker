448e167838df085115d53cdb26e8309bd840dfe4
Jake <folcik.jake@gmail.com>
2019-03-04 15:25:51 -0600

    try this

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,31 +11,76 @@ var chalWords = [
 	"security",
 	"firewall"
 ];
+var wins = "Wins: " + 0;
+var losses = "Losses: " + 0;
+var guesses = "Guesses: " + 7;
+var junkPile = [];
 
 //choose word randomly from array
 var randWord = chalWords[Math.floor(Math.random() * chalWords.length)];
 
-//make underscores based on length of word
-var underscores = [];
-
-for (let i = 0; i < randWord.length; i++) {
-	underscores.push("_");
+var generateNewWord = function(){
+	var randWord = chalWords[Math.floor(Math.random() * chalWords.length)];
 }
 
+console.log(randWord);
+//make underscores based on length of word
+var underscores = "";
+
+for (let i = 0; i < randWord.length; i++) {
+	underscores = underscores + "_";
+}
+console.log(underscores)
+
+
 //get users guess
-document.addEventListener("keypress", event);
 
-console.log(event);
+// if not desired, return google character codes alphabet
+//document.getElementById or getElementByClass
+//
+
+document.onkeyup = function(event){
+	(event.key)
+for (let i = 0; i < randWord.length; i++){
+	if(event.key === randWord[i]){
+		underscore[i] = randWord[i];
+		// html link
+	}if(event.key !== randWord[i]){
+			junkPile.push(event.key);
+			//update html
+			guesses--;
+			//update
+
+		}
+	}
+}
+
+	if(underscores === randWord){
+		wins++;
+		//html
+		generateNewWord;
+	} else
+	if(guesses === 0){
+		losses++;
+		//html
+		generateNewWord;
+	};
 
 
-//compare guess letter and selected word
+// //compare guess letter and selected word
+// (let i = 0; i < random.lenth; i++){
+// 	push(key)
+// })
+
+
+
+
+
 //if found in word push to correct spot in underscore array
 //if wrong push to junk-pile array
 //create wins and losses and guesses variables
 
-var wins = "Wins: " + 0;
-var losses = "Losses: " + 0;
-var guesses = "Guesses: " + 0;
+
 
 //attach them to display in document
 
